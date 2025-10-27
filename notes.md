ee9f461c4726f862ab9b3ee95c0dc8630b6438ce
andynesse <andynesse04@gmail.com>
2025-10-27 14:45:50 +0100

    feat: Add CSRF and initialized bcrypt

--- notes.md
+++ notes.md
@@ -1,1 +1,32 @@
 # Notes Assignment 2
+
+## Changes:
+
+### __init__.py
+
+```py
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+
+login = LoginManager()
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
+...
+def create_app(...):
+    ...
+    login.init_app(app)
+    bcrypt.init_app(app)
+    csrf.init_app(app)
+    ...
+```
+
+
+
+
+## 6. Cryptography
+ 
+ - pip install argon2-cffi
+ - from argon2 import PasswordHasher
+ - ph = PasswordHasher()
+ - ph.hash(<pword>), ph.verify(<hash>, <input_pword>)
--- social_insecurity/__init__.py
+++ social_insecurity/__init__.py
@@ -7,22 +7,28 @@ from pathlib import Path
 from shutil import rmtree
 from typing import cast
 
+
 from flask import Flask, current_app
+
 
 from social_insecurity.config import Config
 from social_insecurity.database import SQLite3
 
-# from flask_login import LoginManager
-# from flask_bcrypt import Bcrypt
-# from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+
+class User(UserMixin):
+    def __init__(self, id: int):
+        self.id = id
+
 
 sqlite = SQLite3()
 # TODO: Handle login management better, maybe with flask_login?
 # login = LoginManager()
 # TODO: The passwords are stored in plaintext, this is not secure at all. I should probably use bcrypt or something
-# bcrypt = Bcrypt()
+bcrypt = Bcrypt()
 # TODO: The CSRF protection is not working, I should probably fix that
-# csrf = CSRFProtect()
+csrf = CSRFProtect()
 
 
 def create_app(test_config=None) -> Flask:
@@ -34,8 +40,8 @@ def create_app(test_config=None) -> Flask:
 
     sqlite.init_app(app, schema="schema.sql")
     # login.init_app(app)
-    # bcrypt.init_app(app)
-    # csrf.init_app(app)
+    bcrypt.init_app(app)
+    csrf = CSRFProtect(app)
 
     with app.app_context():
         create_uploads_folder(app)
@@ -58,3 +64,7 @@ def create_uploads_folder(app: Flask) -> None:
     upload_path = Path(app.instance_path) / cast(str, app.config["UPLOADS_FOLDER_PATH"])
     if not upload_path.exists():
         upload_path.mkdir(parents=True)
+
+def load_user(user_id: str) -> User:
+    """Load a user by ID for Flask-Login."""
+    return User(int(user_id))
