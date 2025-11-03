13f488905a9f3872572160a6571476b7fedd4167
Simen <bodigam112@gmail.com>
2025-11-03 02:53:29 +0100

    dos and unrestricted file upload

--- social_insecurity/config.py
+++ social_insecurity/config.py
@@ -21,7 +21,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "secret"  # TODO: Use this with wtforms
     SQLITE3_DATABASE_PATH = "sqlite3.db"  # Path relative to the Flask instance folder
     UPLOADS_FOLDER_PATH = "uploads"  # Path relative to the Flask instance folder
-    ALLOWED_EXTENSIONS = {}  # TODO: Might use this at some point, probably don't want people to upload any file type
+    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"} #allowed extensions
     WTF_CSRF_ENABLED = False  # TODO: I should probably implement this wtforms feature, but it's not a priority
     
     #Session managment og lockout tiers
@@ -36,3 +36,11 @@ class Config:
         timedelta(minutes=15),
         timedelta(hours=1),
 ]
+
+ # Denial-of-Service protections
+    MAX_CONTENT_LENGTH = 8 * 1024 * 1024  # 8 MB max size of posts/uploads
+
+    # Simple limiter defaults (used by in-process limiter)
+    RATE_LIMIT_REQUESTS = 10     # allowed requests per window (per IP)
+    RATE_LIMIT_WINDOW = 60        # seconds (sliding window)
+    RATE_LIMIT_BLOCK_SECONDS = 300  # seconds to temporarily block IP when exceeded
